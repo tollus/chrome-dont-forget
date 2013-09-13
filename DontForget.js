59dce09c0b1f463c4207aa7e3dcd70a322b5a0e3
dcarps <dcarps@users.noreply.github.com>
2013-09-13 16:47:51 -0400

    Added badge change, badge text, defaults, and window close

--- DontForget.js
+++ DontForget.js
@@ -5,17 +5,15 @@ var DontForgetCtrl = function ($scope, $timeout)
     //defaults
     $scope.ddInOn = ['in', 'on'];
     $scope.ddRepeat = ['never','half hour', 'hour', 'day', 'week', 'year'];
-    $scope.foo = "you foo";
-    $scope.radioModel = 'on';
+    $scope.radioModel = 'in';
     $scope.showWeeks = false;
     var date = new Date();
     var hours = date.getHours();
-    if (hours > 12) {
-        hours -= 12;
-    } else if (hours === 0) {
-        hours = 12;
+
+    var padTime = function(myNum){
+       return myNum < 10 ? "0" + myNum : myNum;
     }
-    $scope.mytime = hours + ":" + date.getMinutes();
+    $scope.mytime = padTime(hours) + ":" + padTime(date.getMinutes());
     $scope.alerts = [];
 
     //datepicker
@@ -45,20 +43,38 @@ var DontForgetCtrl = function ($scope, $timeout)
         {
              var mydate = $scope.dt;
              var mytime = $scope.mytime.split(':');
-             alertDateTime = new Date(mydate.getYear(), mydate.getMonth(), mydate.getDay(), mytime[0], mytime[1], 0, 0)
+             alertDateTime = new Date(mydate.getFullYear(), mydate.getMonth(), mydate.getDay(), mytime[0], mytime[1], 0, 0)
         }else{
-
+            var mydate = $scope.dt;
+            var mytime = $scope.mytime.split(':');
+            alertDateTime = new Date(mydate.getFullYear(), mydate.getMonth(), mydate.getDay(), mytime[0], mytime[1], 0, 0)
         }
+
         if(message == undefined)
             message = "Don't Forget!";
 
         //success = green $scope.alerts.push({type: 'success', msg: new Date(alertDateTime - new Date()) + " " + message});
         //fail = red $scope.alerts.push({type: 'error', msg: new Date(alertDateTime - new Date()) + " " + message});
         //no type = yellow
-        $scope.alerts.push({msg: new Date(alertDateTime - new Date()) + " " + message});
-        $scope.reminderText = '';
+        $scope.alerts.push({msg: alertDateTime + " " + message});
+        chrome.browserAction.setIcon({path: 'DontForget.png'});
+        chrome.browserAction.setBadgeBackgroundColor({color:[255, 255, 255, 0]});
+        chrome.browserAction.setBadgeText({text: $scope.alerts.length.toString()});
+
+        SetDefaults();
     }
     $scope.closeAlert = function(index) {
         $scope.alerts.splice(index, 1);
+        chrome.browserAction.setBadgeText({text: $scope.alerts.length.toString()});
+        SetDefaults();
     };
+
+    function SetDefaults(){
+        $scope.reminderText = '';
+        $scope.selectedRepeat = 'Repeat Every';
+    }
+
+    $scope.CloseWindow = function (){
+        window.close();
+    }
 };
